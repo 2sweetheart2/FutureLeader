841f6757b588cc4c5a663f0308bbe9452388e2db
2sweetheart2 <2004sweetheart2004@gmail.com>
2022-04-27 18:08:02 +0300

    настроил нормально показ фрагмента в окне и сделал динамический список с динамическуим добавлением в него айтемов

--- app/src/main/java/me/solo_team/futureleader/ui/menu/MenuFragment.java
+++ app/src/main/java/me/solo_team/futureleader/ui/menu/MenuFragment.java
@@ -1,8 +1,11 @@
-package me.solo_team.futureleader.ui.dashboard;
+package me.solo_team.futureleader.ui.menu;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -14,15 +17,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
 import me.solo_team.futureleader.R;
 
-public class DashboardFragment extends Fragment {
+public class MenuFragment extends Fragment {
     View root;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        root = inflater.inflate(R.layout.fragment_menu, container, false);
         //создание настроек для сетки
         gridSettings();
         //динамическое добавление элементов в сетку
@@ -42,7 +48,7 @@ public class DashboardFragment extends Fragment {
     }
 
 
-    public void addElement(Bitmap bitmap, String text_) {
+    public void addElement(String urlPhoto, String text_) {
         Point size = new Point();
         requireActivity().getWindowManager().getDefaultDisplay().getSize(size);
         int halfScreenWidth = (int) (size.x * 0.5);
@@ -55,19 +61,35 @@ public class DashboardFragment extends Fragment {
             column = 1;
             row = (int) offest / 2;
         }
-        System.out.println("ADD ELEMT IN " + column + " " + row + " (" + offest + ")");
         offest++;
         GridLayout.Spec col_ = GridLayout.spec(column);
         GridLayout.Spec row_ = GridLayout.spec(row);
         GridLayout.LayoutParams lp = new GridLayout.LayoutParams(row_, col_);
         TextView text = new TextView(root.getContext());
         lp.width = halfScreenWidth;
+        lp.height = lp.width;
         text.setLayoutParams(lp);
         text.setGravity(Gravity.CENTER);
         text.setBackgroundColor(Color.WHITE);
         text.setText(text_);
         text.setTextAppearance(root.getContext(), android.R.style.TextAppearance_Large_Inverse);
-        text.setBackground(requireContext().getDrawable(R.drawable.ic_launcher_background));
+        if(urlPhoto!=null) Picasso.get().load(Uri.parse(urlPhoto)).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                text.setBackground(new BitmapDrawable(bitmap));
+            }
+
+            @Override
+            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        });
+        text.setBackground(requireContext().getDrawable(R.drawable.gray_gradiernt));
         gridLayout.addView(text, lp);
     }
 }
---
+++ app/src/main/java/me/solo_team/futureleader/ui/news/NewsFragment.java
@@ -0,0 +1,23 @@
+package me.solo_team.futureleader.ui.news;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import me.solo_team.futureleader.R;
+
+public class NewsFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_news, container, false);
+
+        return root;
+    }
+}
